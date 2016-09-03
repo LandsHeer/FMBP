cfa8faf56c72b0dee7af8d718ad1253f673762e6
DESKTOP-1I8PHAM\Patrick <pvoelz@mail.de>
2016-09-03 14:58:28 +0200

    Base Settings + DemoMap added

---
+++ FMBP/Source/FMBP/FMBPGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FMBPGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FMBP_API AFMBPGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
